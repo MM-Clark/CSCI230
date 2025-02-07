70b06d8222521572e92d08a6d0f87f677f4739af
Michelle Clark <142937692+MM-Clark@users.noreply.github.com>
2025-02-07 08:20:28 -0500

    Add files via upload
    A simple guesser game implementing a stack. In progress.

---
+++ JumpGameWithStack/src/App.java
@@ -0,0 +1,8 @@
+// Written by M.M.Clark
+// 2/7/2025
+public class App {
+    protected static Runner game = new Runner();
+    public static void main(String[] args) throws Exception {
+        game.start();
+    }
+}
