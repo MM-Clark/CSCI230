ef4adcbcbeeb9646675a25112d9bfae8ed6f302e
Michelle Clark <142937692+MM-Clark@users.noreply.github.com>
2025-02-26 13:22:39 -0500

    Add files via upload

--- STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/src/Gui.java
+++ STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/src/Gui.java
@@ -5,45 +5,73 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.*;
 
+// provides user interface functionality of when to add/remove JPanels, etc; also for pushing/popping stack
 public class Gui implements ActionListener
 {
-    private JButton start;
     private JFrame frame;
+    
+    private JButton start; 
+
+    private JRadioButton yes;
+    private JRadioButton no;
+
+    private ButtonGroup yesNo;
+
     private JPanel startScreen;
+    private JPanel guessScreen;
 
-    public Gui() // creates GUI initially
+    private JLabel prompt;
+    private JLabel continueTower;
+    private JLabel welcome;
+
+    // private JPanel towerPanel; 
+
+    private StackArray<Block> stackTower;
+    private WinLoseScreen wls;
+    TowerBlock t;
+
+    // creates GUI initially, sets up JFrame
+    public Gui() 
     {
+        // declarations
         frame = new JFrame();
+        startScreen = new JPanel();
+        guessScreen = new JPanel();
+
+        wls = new WinLoseScreen();
+        t = new TowerBlock();
+
+        // frame set up
         frame.setTitle("Start Screen");
-        // frame.setSize(900, 900);
+        frame.setSize(900, 900); //--------> manually set size, easier for placing boxes on x/y coordinates
+        getStartPanel(); // calls method below to get start panel
+        showStartScreen();
 
-        JPanel startScreen = getStartPanel();
-        frame.add(startScreen);
-
-        frame.pack();                         // if this is taken out, must put back in set size
+        // frame.pack();            //---------> predetermined size by system
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
     }
 
-    private JPanel getStartPanel() // start Screen JPanel
+    private void getStartPanel() // start Screen JPanel
     {
-        startScreen = new JPanel();
+        // ---------- set up startScreen --------------
         startScreen.setLayout(new BoxLayout(startScreen, BoxLayout.Y_AXIS)); // to make stuff align vertically
         startScreen.setBackground(Color.BLACK);
-
         //startScreen.add(Box.createVerticalGlue());        //-------> in case tries to get uncentered
-
-        JLabel welcome = new JLabel("Welcome to Stack Jenga Horror Game!"); // this text can definitely be edited
+        
+        // welcome message
+        welcome = new JLabel("Welcome to Stack Jenga Horror Game!"); // this text can definitely be edited
         welcome.setForeground(Color.RED); // so can any colors used, these are just filler for now
         welcome.setAlignmentX(Component.CENTER_ALIGNMENT); // Center horizontally
-
         startScreen.add(welcome);
 
-        startScreen.add(Box.createVerticalStrut(30)); // Add vertical space
+        // Add vertical space
+        startScreen.add(Box.createVerticalStrut(30)); 
 
+        // start button
         start = new JButton("START");
         start.setPreferredSize(new Dimension(1200, 500));
         start.setBackground(Color.RED);
@@ -52,37 +80,156 @@ public class Gui implements ActionListener
         startScreen.add(start);
 
         //startScreen.add(Box.createVerticalGlue());    //---> in case tries to get uncentered
-
-        return startScreen;
     }
 
-    private void getGuessPanel() // for when user is stacking the tower
+    private void showStartScreen()
     {
-        JPanel guess = new JPanel();
-        guess.setBackground(Color.WHITE);
-        startScreen.setLayout(new BoxLayout(startScreen, BoxLayout.Y_AXIS)); // to make stuff align vertically
-
-        JLabel prompt = new JLabel("A random number has been assigned to you. Build the tower to the number.");
-        prompt.setAlignmentX(Component.CENTER_ALIGNMENT); // Center horizontally
-        guess.add(prompt);
-
-        startScreen.add(Box.createVerticalStrut(30)); // Add vertical space
-
-        JLabel continueTower = new JLabel("\nDo you want to keep building the tower?"); // will fix dialogue with int i + for loop later
-        continueTower.setAlignmentX(Component.CENTER_ALIGNMENT); // Center horizontally --> THIS NEEDS TO BE FIXED
-        guess.add(continueTower);
-
-        frame.setVisible(false);
-        frame.remove(startScreen);
-        frame.add(guess);
-
+        frame.getContentPane().removeAll();
+        frame.add(startScreen);
         frame.setVisible(true);
     }
 
+    // gets random stack size
+    private void getStackSize()
+    {
+        // --------------- RANDOMLY DETERMINED SIZE OF STACK -----------------------------------------
+        //-----------*********will need tweaking to improve*******--------------------
+        int min = 5; // definitely needs adjustment
+        int max = 10; // also needs adjustment
+        int rangeRandom = min + (int)(Math.random() * ((max - min) + 1));
+        int stackSize = rangeRandom;
+        stackTower = new StackArray<Block>(stackSize);
+    }
+
+    // sets up GuessScreen JPanel 
+    private void getGuessPanel() // for when user is stacking the tower
+    {
+        //----------set up guess panel--------------------
+        guessScreen.setBackground(Color.BLACK);
+        startScreen.setLayout(new BoxLayout(startScreen, BoxLayout.Y_AXIS)); // to make stuff align vertically
+
+        // user directions
+        prompt = new JLabel("A random number has been assigned to you. Build the tower to the number.");
+        prompt.setForeground(Color.RED);
+        prompt.setAlignmentX(Component.CENTER_ALIGNMENT); // Center horizontally
+        guessScreen.add(prompt);
+
+        // Add vertical space
+        guessScreen.add(Box.createVerticalStrut(30)); 
+
+        //******************************************************************************************************** */
+        // ***** user prompt to keep building, HAVE HAD ISSUES GETTING BELOW PROMPT JLABEL ************* 
+        continueTower = new JLabel("\nDo you want to keep building the tower?"); // will fix dialogue with int i + for loop later
+        continueTower.setForeground(Color.WHITE);
+        continueTower.setAlignmentX(Component.CENTER_ALIGNMENT); // Center horizontally --> THIS NEEDS TO BE FIXED
+        guessScreen.add(continueTower);
+        //******************************************************************************************************** */
+
+        // Add vertical space
+        guessScreen.add(Box.createVerticalStrut(30)); 
+
+        // yes option
+        yes = new JRadioButton("Yes! Keep Going");
+        yes.addActionListener(this); // adds action listener to get user input 
+        guessScreen.add(yes); // *****    SEE END OF CODE FOR ACTION LISTENER METHOD   **********
+
+        // no option
+        no = new JRadioButton("No. Stop.");
+        no.addActionListener(this);
+        guessScreen.add(no);
+
+        // grouping yes/no buttons
+        yesNo = new ButtonGroup();
+        yesNo.add(yes);
+        yesNo.add(no);
+
+        guessScreen.add(yes);
+        guessScreen.add(no);
+    }
+
+    private void showGuessScreen()
+    {
+        // updating screen w/o glitching
+        frame.getContentPane().removeAll();;
+        frame.add(guessScreen);
+        frame.revalidate();
+    }
+
+    // action listener calls if user chooses yes
+    public void yesAction() throws InterruptedException, IOException
+    {
+        Block newBlock = new Block(1, "block"); // temp block to put in stack
+        if(!stackTower.isFull())
+            stackTower.push(newBlock); // stack tower is the stack
+        else
+        {
+            System.out.println("Stack full...."); // error check
+            userLoses();
+        }
+        //********************************************************************************************************* */
+        // switches to tower screen ***** TOWERBLOCK CLASS NEEDS TO INCREASE HEIGHT OVER TIME *********
+        //***************************************************************************************************** */
+        t.go(frame);
+
+        //***************************************************************************************************** */
+        // Thread.sleep(1000); // to put in delay to show tower screen --> CAUSES TOWER TO NOT SHOW RIGHT NOW
+        // showGuessScreen();
+        //********************************************************************************************************* */
+    }
+
+    public void noAction() throws IOException
+    {
+        if(!stackTower.isFull())
+        {
+           System.out.println("User guessed less than stack size."); // error checking
+           userLoses();
+        }
+        else if(stackTower.isFull())
+        {
+            System.out.println("User guessed correct stack size."); // error checking
+            userWins();
+        }
+    }
+
+    public void userLoses() throws IOException
+    {
+        wls.showLoseScreen(frame);
+        // System.out.println("User loses!!!!");
+        // System.exit(0); // maybe should make replay later on 
+    }
+
+    public void userWins() throws IOException
+    {
+        wls.showWinScreen(frame);
+        // System.out.println("User wins!!!");
+        // System.exit(0);
+    }
+
     @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        if(e.getSource() == start)
+    public void actionPerformed(ActionEvent e) // for making buttons do things
+    {
+        if(e.getSource() == start) // start screen start button
+        {
+            getStackSize();
             getGuessPanel();
+            showGuessScreen();
+        }
+        else if(e.getSource() == yes)
+        {
+            try {
+                yesAction();
+            } catch (InterruptedException | IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+        else if(e.getSource() == no)
+        {
+            try {
+                noAction();
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        }
     }
 }
